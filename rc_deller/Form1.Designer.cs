74912dd90913869588ed402ff743b4245d6bf684
oskar <oskar@mangosia.com>
2017-06-28 11:37:35 +0200

    endra størelksen på vinduet

--- rc_deller/Form1.Designer.cs
+++ rc_deller/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // progressBar1
             // 
             this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.progressBar1.Location = new System.Drawing.Point(680, 508);
+            this.progressBar1.Location = new System.Drawing.Point(684, 475);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(132, 11);
             this.progressBar1.TabIndex = 0;
@@ -44,7 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(824, 531);
+            this.ClientSize = new System.Drawing.Size(828, 498);
             this.Controls.Add(this.progressBar1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
