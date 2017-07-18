871838aad0bb400527f4b8f92c20b7f2fe5d3b1e
oskar <oskar@mangosia.com>
2017-07-18 07:49:41 +0200

    låste en knap til en posison

--- rc_deller/add deler.Designer.cs
+++ rc_deller/add deler.Designer.cs
@@ -52,7 +52,7 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(315, 94);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(221, 20);
             this.textBox1.TabIndex = 1;
