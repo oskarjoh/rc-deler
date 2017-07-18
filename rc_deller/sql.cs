34b042fe7ff9224546491dd4e8c443d3a14144b5
oskar <oskar@mangosia.com>
2017-07-18 06:37:29 +0200

    lagt til sql

--- rc_deller/sql.cs
+++ rc_deller/sql.cs
@@ -7,8 +7,24 @@ using System.Data.SqlClient;
 
 namespace rc_deller
 {
+
     public class sql
     {
+        public void Sql()
+        {
+            // Create the connection to the resource!
+            // This is the connection, that is established and
+            // will be available throughout this block.
+            using (SqlConnection conn = new SqlConnection())
+            {
+                // Create the connectionString
+                // Trusted_Connection is used to denote the connection uses Windows Authentication
+                conn.ConnectionString = "Server=[192.168.0.106];Database=[rc-deler];Trusted_Connection=true";
+                conn.Open();
+                // Create the command
+                SqlCommand command = new SqlCommand("SELECT * FROM rom", conn);
 
+            }
+        }
     }
 }
