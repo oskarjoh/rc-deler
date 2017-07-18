9e498c7978dc28e0301d55d2a488af3ed335e6a3
oskar <oskar@mangosia.com>
2017-07-18 15:35:09 +0200

    added progresbar

--- rc_deller/less deller.Designer.cs
+++ rc_deller/less deller.Designer.cs
@@ -28,13 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(856, 508);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(163, 18);
+            this.progressBar1.TabIndex = 0;
             // 
             // less_deller
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1031, 538);
+            this.Controls.Add(this.progressBar1);
             this.Name = "less_deller";
             this.Text = "less_deller";
             this.ResumeLayout(false);
@@ -42,5 +51,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
