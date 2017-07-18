5c4232a35b88f6fc932c5362f65f24707e2a49d8
oskar <oskar@mangosia.com>
2017-07-18 10:23:22 +0200

    lagt til sumit knap

--- rc_deller/add deler.cs
+++ rc_deller/add deler.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace rc_deller
 {
@@ -44,7 +45,20 @@ namespace rc_deller
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // Create the connection to the resource!
+            // This is the connection, that is established and
+            // will be available throughout this block.
+            using (SqlConnection conn = new SqlConnection())
+            {
+                // Create the connectionString
+                // Trusted_Connection is used to denote the connection uses Windows Authentication
+                conn.ConnectionString = "Server=[192.168.0.106];Database=[rc-deler];Trusted_Connection=true";
+                conn.Open();
+                // Create the command
+                SqlCommand command = new SqlCommand("SELECT * FROM rom", conn);
 
+            }
         }
+    }
     }
 }
