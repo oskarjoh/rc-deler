96abf8319f16e4608351aed60989167cf9f79095
oskar <oskar@mangosia.com>
2017-07-20 18:46:01 +0200

    lagt til den delen som leser koden

--- rc_deller/less deller.cs
+++ rc_deller/less deller.cs
@@ -109,5 +109,51 @@ namespace rc_deller
                 return false;
             }
         }
+
+        //Select statement
+        public List<string>[] Select()
+        {
+            string query = "SELECT * FROM rom";
+
+            //Create a list to store the result
+            List<string>[] list = new List<string>[3];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+            list[2] = new List<string>();
+            list[3] = new List<string>();
+            list[4] = new List<string>();
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    list[0].Add(dataReader["deler_navn"] + "");
+                    list[1].Add(dataReader["produkt_nommer"] + "");
+                    list[2].Add(dataReader["skof_id"] + "");
+                    list[3].Add(dataReader["rom_navn"] + "");
+                    list[4].Add(dataReader["antall"] + "");
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
+                //close Connection
+                this.CloseConnection();
+
+                //return list to be displayed
+                return list;
+            }
+            else
+            {
+                return list;
+            }
+        }
     }
 }
