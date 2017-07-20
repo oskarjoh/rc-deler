1a44429800ca9e9c4252109f0bd51cac9fe17ec3
oskar <oskar@mangosia.com>
2017-07-20 20:02:20 +0200

    endra størelsen

--- rc_deller/abote.Designer.cs
+++ rc_deller/abote.Designer.cs
@@ -34,7 +34,7 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button1.Location = new System.Drawing.Point(13, 409);
+            this.button1.Location = new System.Drawing.Point(13, 495);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 27);
             this.button1.TabIndex = 0;
@@ -46,7 +46,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(606, 448);
+            this.ClientSize = new System.Drawing.Size(1052, 534);
             this.Controls.Add(this.button1);
             this.Name = "abote";
             this.Text = "abote";
