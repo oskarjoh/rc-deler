411675e3f29534d68f0512639d7493243e90f191
oskar <oskar@mangosia.com>
2017-07-20 19:21:11 +0200

    byta bakruns farge

--- rc_deller/less deller.Designer.cs
+++ rc_deller/less deller.Designer.cs
@@ -47,6 +47,7 @@
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(17, 16);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
